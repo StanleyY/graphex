6bdc7507183dd870ffb3f40320c126d46f562ca7
StanleyY <stanley.yang.sy@gmail.com>
2015-04-28 18:41:22 -0400

    Basic NFA generation working

---
+++ graphex/NFA.java
@@ -0,0 +1,103 @@
+package graphex;
+
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class NFA{
+
+  String EDGE_DOT_FORMAT = "%s -> %s[label=\"%s\"];";
+
+  NFAnode root;
+  int nodeNumber = 0;
+  char[] input;
+  int parsePosition = 0;
+
+  public NFA(String regex){
+    try{
+      input = regex.toCharArray();
+      root = generateNFA();
+    } catch (InvalidException e){
+      e.printStackTrace();
+      System.exit(1);
+    }
+  }
+
+  private class NFAnode{
+    int number;
+    HashMap<Character, ArrayList<NFAnode>> edges = new HashMap<Character, ArrayList<NFAnode>>();
+
+    public NFAnode(int nodeNumber){
+      number = nodeNumber;
+    }
+
+    public void addEdge(char symbol, NFAnode node){
+      if(edges.get(symbol) == null){
+        edges.put(symbol, new ArrayList<NFAnode>());
+      }
+      edges.get(symbol).add(node);
+    }
+  }
+
+  private class InvalidException extends Exception {
+    public InvalidException() {
+      super("Invalid Regex");
+    }
+  }
+
+  private NFAnode generateNFA() throws InvalidException{
+    NFAnode start = new NFAnode(nodeNumber);
+    nodeNumber++;
+
+    if(input[parsePosition] == '('){
+      parseParens(start);
+    }
+    else {
+      parseChar(start);
+    }
+    return start;
+  }
+
+  private void parseChar(NFAnode start){
+    NFAnode end = new NFAnode(nodeNumber);
+    nodeNumber++;
+    System.out.println("adding " + input[parsePosition]);
+    start.addEdge(input[parsePosition], end);
+    parsePosition++;
+    parseNext(end);
+  }
+
+  private void parseNext(NFAnode start){
+    if(parsePosition < input.length){
+      System.out.println("PARSING NEXT");
+      parseChar(start);
+    }
+  }
+
+  private void parseParens(NFAnode start){
+    parsePosition++; //Consume the open parens
+  }
+
+  public void generateDOTfile(){
+    try{
+      PrintWriter output = new PrintWriter("NFA.dot");
+      output.println("digraph {");
+      generateNodeDOT(output, root);
+      output.println("}");
+      output.close();
+    } catch (java.io.IOException e){
+      e.printStackTrace();
+      System.exit(1);
+    }
+  }
+
+  private void generateNodeDOT(PrintWriter out, NFAnode node){
+    for(Character transition : node.edges.keySet()){
+      for(NFAnode n : node.edges.get(transition)){
+        out.println(String.format(EDGE_DOT_FORMAT, node.number, n.number, transition));
+        generateNodeDOT(out, n);
+      }
+    }
+  }
+
+}
