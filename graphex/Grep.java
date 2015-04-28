361f8d3359e57f812955bee3ad3435b2644cc3da
StanleyY <stanley.yang.sy@gmail.com>
2015-04-28 01:05:04 -0400

    added reading file line by line

--- graphex/Grep.java
+++ graphex/Grep.java
@@ -1,7 +1,23 @@
 package graphex;
 
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.io.BufferedReader;
+
 class Grep{
+
   public static void main(String[] args){
-    System.out.println("Hello");
+    System.out.println("Regex was: " + args[0]);
+    try {
+      FileInputStream input = new FileInputStream(args[1]);
+      BufferedReader br = new BufferedReader(new InputStreamReader(input));
+      String line;
+      while((line = br.readLine()) != null) {
+        System.out.println(line);
+      }
+    } catch (java.io.IOException e) {
+      e.printStackTrace();
+      System.exit(1);
+    }
   }
 }
